501fd39c16daf1668c820c1c04f8750848aac7c5
B.Viktor <benkovik@gmail.com>
2021-04-30 11:05:04 +0200

    Task 01-02 completed

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,21 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
 
+    document.querySelectorAll('a').forEach(link => {
+        if (link.innerHTML.includes('outer-link')) {
+            link.setAttribute('target', '_blank');
+            link.innerHTML = `<strong>${link.innerHTML}</strong>`;
+        }
+    })
+
+    document.querySelectorAll('nav').forEach(item => {
+        item.style.display = 'flex';
+        item.style.flexDirection = 'column';
+        item.style.width = '30%';
+        item.style.margin = '0px auto';
+        item.style.border = '1px solid blue';
+        item.style.padding = '16px';
+    });
+
+};
 
 export { changeOuterLinks };
